0e1ec409e08fbc8a7a4c51a1aca21e04307e8375
omidatglo <omid@glo3d.com>
2023-09-10 00:21:09 +0330

    added link to email

--- src/felicinema/helpers/emailing.py
+++ src/felicinema/helpers/emailing.py
@@ -2,12 +2,6 @@ from django.conf import settings
 from django.core.mail import send_mail, EmailMessage
 
 
-# subject = 'welcome to GFG world'
-# message = f'Hi {user.username}, thank you for registering in geeksforgeeks.'
-# email_from = settings.EMAIL_HOST_USER
-# recipient_list = [user.email, ]
-# send_mail( subject, message, email_from, recipient_list )
-
 class Mailer:
     from felicinema.apps.cinema.models import Payment
 
@@ -22,7 +16,8 @@ class Mailer:
                   f'from: {payment.ticket.user} \n' \
                   f'the security code for this reservation is: {payment.code}\n' \
                   f'\n' \
-                  f'To accept the reservation please go to the link below and enter the security code, \n' \
+                  f'To accept the reservation please go to the link below and enter the security code: \n' \
+                  f'cinema/reservation/{payment.id}/' \
                   f'Or you can check your dashboard and accept the requests you want. \n' \
                   f'Regards, \n' \
                   f'Omid from FeliCinema'
